55bc8d53bd6f47483efa52dd859f522ba70758e4
cela <1319239848@qq.com>
2019-07-25 18:21:20 +0800

    二叉树层次遍历，使用队列先进先出的方式

---
+++ src/leetcode102.java
@@ -0,0 +1,46 @@
+import javax.xml.soap.Node;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @auther ZhangKe
+ * @date 2019/7/25 18:11
+ * 二叉树的层次遍历
+ */
+public class leetcode102 {
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode(int x) { val = x; }
+     * }
+     */
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        if (root == null) {
+            return result;
+        }
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int num = queue.size();
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < num; i++) {
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            result.add(list);
+        }
+        return result;
+    }
+}
