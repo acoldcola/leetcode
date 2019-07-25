c6b4dfad7fcc07d33bd0d152364a1cf15c962171
cela <1319239848@qq.com>
2019-07-25 16:40:37 +0800

    跳跃游戏，贪心算法

---
+++ src/leetcode55.java
@@ -0,0 +1,17 @@
+/**
+ * @auther ZhangKe
+ * @date 2019/7/25 16:25
+ * 跳跃游戏
+ */
+public class leetcode55 {
+    public boolean canJump(int[] nums) {
+        int x = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (i > x || x > nums.length -1) {
+                break;
+            }
+            x = x > nums[i] + i ? x : nums[i] + i;
+        }
+        return x > nums.length - 1;
+    }
+}
