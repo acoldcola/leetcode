bf56170ee336337c186fff4403f8c30123a60540
cela <1319239848@qq.com>
2019-07-26 16:06:04 +0800

    多添加一个为0的数

--- src/leetcode84.java
+++ src/leetcode84.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Stack;
 
 /**
@@ -7,19 +8,21 @@ import java.util.Stack;
  */
 public class leetcode84 {
     public static void main(String[] args) {
-        int[] a = {2, 1, 5, 6, 2, 3};
+        int[] a = {1,1,3};
         System.out.println(largestRectangleArea(a));
     }
 
-    public static int largestRectangleArea(int[] height) {
-        if (height.length == 0 || height == null) {
+    public static int largestRectangleArea(int[] heights) {
+        if (heights.length == 0 || heights == null) {
             return 0;
         }
+        // 需要末尾添加一个0，最后把栈里的数据全部导出来
+        int[] height = Arrays.copyOf(heights, heights.length + 1);
         int area = 0;
         Stack<Integer> stack = new Stack<>();
         for (int i = 0; i < height.length; i++) {
             // 判断是否为空并且数是否小于栈顶的数
-            while (!stack.isEmpty() && height[i] < height[stack.peek()]) {
+            while (!stack.isEmpty() && height[i] <= height[stack.peek()]) {
                 // 算每个矩形的面积
                 int x = height[stack.pop()]
                         * (stack.isEmpty() ? i : i - (stack.peek() + 1));
