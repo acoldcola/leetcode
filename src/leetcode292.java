5036a20b4f8d063f174be96fefdc9c07a932836c
cela <1319239848@qq.com>
2019-07-26 17:57:56 +0800

    Nim游戏

---
+++ src/leetcode292.java
@@ -0,0 +1,13 @@
+/**
+ * @auther ZhangKe
+ * @date 2019/7/26 17:51
+ * Nim 游戏
+ */
+public class leetcode292 {
+    public boolean canWinNim(int n) {
+        if(n % 4 == 0){
+            return false;
+        }
+        return true;
+    }
+}
