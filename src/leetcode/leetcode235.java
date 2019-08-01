25ea7fdc79a06cd41ba3a28969ebade12d5ad1f1
cela <1319239848@qq.com>
2019-08-01 18:05:46 +0800

    二叉搜索树的最近公共祖先

--- src/leetcode/leetcode235.java
+++ src/leetcode/leetcode235.java
@@ -3,6 +3,7 @@ package leetcode;
 /**
  * @auther ZhangKe
  * @date 2019/8/1 17:38
+ * 二叉搜索树的最近公共祖先
  */
 public class leetcode235 {
     /**
@@ -16,6 +17,12 @@ public class leetcode235 {
      */
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        return null;
+        if (p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestor(root.left, p , q);
+        }
+        if (p.val > root.val && q.val > root.val) {
+            return lowestCommonAncestor(root.right, p , q);
+        }
+        return root;
     }
 }
