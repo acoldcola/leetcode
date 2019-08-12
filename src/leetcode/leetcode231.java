8aa199332c44c48a6a275f89da59ee0571e7f4a0
cela <1319239848@qq.com>
2019-08-12 17:45:01 +0800

    用&方法解决2的幂

---
+++ src/leetcode/leetcode231.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+/**
+ * @auther ZhangKe
+ * @date 2019/8/12 17:30
+ * 2的幂
+ */
+public class leetcode231 {
+    public boolean isPowerOfTwo(int n) {
+        if (n < 1) {
+            return false;
+        }
+        else if (n > 1) {
+            if ((n&(n-1)) == 0) {
+                return true;
+            }
+        }
+        else if (n == 1) {
+            return true;
+        }
+        return false;
+    }
+}
