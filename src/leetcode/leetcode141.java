d11dc8cb4bceef35b7f545e9a41eab7b6abad31a
cela <1319239848@qq.com>
2019-08-12 15:32:16 +0800

    快慢指针解决环形链表

--- src/leetcode/leetcode141.java
+++ src/leetcode/leetcode141.java
@@ -7,6 +7,18 @@ package leetcode;
  */
 public class leetcode141 {
     public boolean hasCycle(ListNode head) {
-        return true;
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode h1 = head;
+        ListNode h2 = head;
+        while (h2.next != null && h2.next.next != null) {
+           h1 = h1.next;
+           h2 = h2.next.next;
+           if (h1 == h2) {
+               return true;
+           }
+        }
+        return false;
     }
 }
