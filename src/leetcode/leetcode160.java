140ae504e7c308f75788247e2c0377808b76ebc4
cela <1319239848@qq.com>
2019-08-12 15:02:08 +0800

    相交链表

---
+++ src/leetcode/leetcode160.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+/**
+ * @auther ZhangKe
+ * @date 2019/8/12 14:43
+ * 相交链表
+ */
+public class leetcode160 {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        ListNode x = headA;
+        ListNode y = headB;
+        while (x != y) {
+            x = x == null ? headB : headA.next;
+            y = y == null ? headA : headB.next;
+        }
+        return x;
+    }
+}
