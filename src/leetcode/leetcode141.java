b45ce71f8858d4e07fd4174a23b1acc59b6c8e60
cela <1319239848@qq.com>
2019-08-12 15:01:24 +0800

    相交链表

---
+++ src/leetcode/leetcode141.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+/**
+ * @auther ZhangKe
+ * @date 2019/8/2 18:16
+ * 环形链表
+ */
+public class leetcode141 {
+    public boolean hasCycle(ListNode head) {
+        return true;
+    }
+}
