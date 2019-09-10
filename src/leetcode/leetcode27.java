a889b740255a045481d16284183a4cd682cadfe9
cela <1319239848@qq.com>
2019-09-10 18:12:59 +0800

    双指针解决移除元素

---
+++ src/leetcode/leetcode27.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+/**
+ * @auther ZhangKe
+ * @date 2019/9/10 17:50
+ * 移除元素
+ */
+public class leetcode27 {
+    public static void main(String[] args){
+        int[] a = {3,3};
+        removeElement(a,5);
+    }
+
+    public static int removeElement(int[] nums, int val) {
+        if (nums == null) {
+            return 0;
+        }
+        int fast = 0;
+        int slow = 0;
+        while (fast < nums.length) {
+            if (nums[fast] == val) {
+                fast ++;
+                continue;
+            }
+            nums[slow++] = nums[fast++];
+        }
+        return slow;
+    }
+}
