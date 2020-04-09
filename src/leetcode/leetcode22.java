c08694e166a2ae074d0a7c5f333a6fd50cef2661
cela <1319239848@qq.com>
2020-04-09 10:19:58 +0800

    使用dfs完成括号生成

--- src/leetcode/leetcode22.java
+++ src/leetcode/leetcode22.java
@@ -1,5 +1,6 @@
 package leetcode;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -8,7 +9,21 @@ import java.util.List;
  * 括号生成
  */
 public class leetcode22 {
+    List<String> strings = new ArrayList<>();
     public List<String> generateParenthesis(int n) {
-        return null;
+        dfs(n,n,"");
+        return strings;
+    }
+    public void dfs(int left,int right,String str) {
+        if (left == 0 && right == 0) {
+            strings.add(str);
+            return;
+        }
+        if (left > 0) {
+            dfs(left - 1,right,str + "(");
+        }
+        if (right > left) {
+            dfs(left,right - 1,str + ")");
+        }
     }
 }
---
+++ src/leetcode/leetcode860.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * @Author ZhangKe
+ * @Date 2020/4/8 17:56
+ * @Version 1.0
+ * 柠檬水找零
+ */
+public class leetcode860 {
+    public boolean lemonadeChange(int[] bills) {
+        for (int i = 0; i < bills.length; i++) {
+            if (bills[i] == 5) {
+                
+            }
+        }
+        return false;
+    }
+}
