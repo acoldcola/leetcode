aa90b209c9ba96d8914fc50ba96af56e7ce958b3
cela <1319239848@qq.com>
2020-06-19 15:00:27 +0800

    最近的请求次数

--- src/leetcode/leetcode933.java
+++ src/leetcode/leetcode933.java
@@ -1,5 +1,8 @@
 package leetcode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @Author ZhangKe
  * @Date 2020/4/13 17:59
@@ -7,12 +10,16 @@ package leetcode;
  * 最近的请求次数
  */
 public class leetcode933 {
-
+    Queue<Integer> queue;
     public leetcode933() {
-
+        queue = new LinkedList<>();
     }
 
     public int ping(int t) {
-        return 0;
+        queue.add(t);
+        while (queue.peek() < t - 3000){
+            queue.poll();
+        }
+        return queue.size();
     }
 }
