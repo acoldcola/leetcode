3752cf7d983c6e99251d2af1cd74b8d960db84de
cela <1319239848@qq.com>
2020-07-01 21:13:12 +0800

    最大数值

---
+++ src/leetcode/interview16_07.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+/**
+ * @Author ZhangKe
+ * @Date 2020/7/1 21:07
+ * @Version 1.0
+ * 最大数值
+ */
+public class interview16_07 {
+    public int maximum(int a, int b) {
+        return a > b ? a : b;
+    }
+}
