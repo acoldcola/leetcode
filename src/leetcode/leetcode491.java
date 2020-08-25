bf50761ca16eec03f804839a99fb1ffb4a15f901
cela <1319239848@qq.com>
2020-08-25 11:27:55 +0800

    回溯解决递增子序列

--- src/leetcode/leetcode491.java
+++ src/leetcode/leetcode491.java
@@ -1,7 +1,9 @@
 package leetcode;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * @Author ZhangKe
@@ -10,8 +12,37 @@ import java.util.List;
  *  递增子序列
  */
 public class leetcode491 {
+
+    private List<List<Integer>> res = new ArrayList<List<Integer>>();
+    private List<Integer> temp = new ArrayList<>();
+
+    public static void main(String[] args) {
+        leetcode491 l = new leetcode491();
+        l.findSubsequences(new int[] {1,2,3,4,5,4,1,1,1,1});
+    }
     public List<List<Integer>> findSubsequences(int[] nums) {
-        List<List<Integer>> allList = new ArrayList<>();
-        return null;
+        if (nums == null) {
+            return null;
+        }
+        dfs(0, Integer.MIN_VALUE, nums);
+        return res;
+    }
+
+    private void dfs(int curIndex, int preValue, int[] nums) {
+        if (curIndex >= nums.length) {
+            if (temp.size() >= 2) {
+                res.add(new ArrayList<>(temp));
+            }
+            return;
+        }
+
+        if (nums[curIndex] >= preValue) {
+            temp.add(nums[curIndex]);
+            dfs(curIndex + 1, nums[curIndex], nums);
+            temp.remove(temp.size() - 1);
+        }
+        if (nums[curIndex] != preValue) {
+            dfs(curIndex + 1, preValue, nums);
+        }
     }
 }
