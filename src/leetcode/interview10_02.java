93e3839572e2352b55724805d7e8bb37c09721d1
cela <1319239848@qq.com>
2020-09-09 13:58:31 +0800

    回溯解决组数总和

---
+++ src/leetcode/interview10_02.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+import java.util.List;
+
+/**
+ * @Author ZhangKe
+ * @Date 2020/9/7 22:09
+ * @Version 1.0
+ * 变位词组
+ */
+public class interview10_02 {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+    }
+}
--- src/leetcode/leetcode39.java
+++ src/leetcode/leetcode39.java
@@ -1,5 +1,6 @@
 package leetcode;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -8,7 +9,26 @@ import java.util.List;
  * 组合总和
  */
 public class leetcode39 {
+    List<List<Integer>> lists = new ArrayList<>();
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        return null;
+        dfs(0,0,candidates,target,new ArrayList<>());
+        return lists;
+    }
+
+    private void dfs(int begin,int sum, int[] candidates, int target, List<Integer> list) {
+        if (sum > target){
+            return;
+        }
+        if (sum == target){
+            lists.add(new ArrayList<>(list));
+            return;
+        }
+        for (int i = begin; i < candidates.length; i++){
+            sum += candidates[i];
+            list.add(candidates[i]);
+            dfs(i,sum,candidates,target,list);
+            sum -= candidates[i];
+            list.remove(list.size() - 1);
+        }
     }
 }
