9ca5780297f980c13d3a67f13a59b76980d6d175
cela <1319239848@qq.com>
2020-12-14 17:47:47 +0800

    存在重复元素

--- src/leetcode/leetcode217.java
+++ src/leetcode/leetcode217.java
@@ -15,9 +15,10 @@ public class leetcode217 {
         for (int i = 0; i < nums.length; i++) {
             if (map.containsKey(nums[i])) {
                 return true;
-            }else {
-                map.put(nums[i],1);
+            } else {
+                map.put(nums[i], 1);
             }
         }
         return false;
+    }
 }
