061d585e2736a54e4047f2eefc008c71c5e3cffb
cela <1319239848@qq.com>
2020-12-18 11:51:23 +0800

    变位词组

--- src/leetcode/interview10_02.java
+++ src/leetcode/interview10_02.java
@@ -1,6 +1,6 @@
 package leetcode;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * @Author ZhangKe
@@ -9,7 +9,19 @@ import java.util.List;
  * 变位词组
  */
 public class interview10_02 {
+    public static void main(String[] args) {
+        interview10_02 l = new interview10_02();
+        l.groupAnagrams(new String[] {"eat", "tea", "tan", "ate", "nat", "bat"});
+    }
     public List<List<String>> groupAnagrams(String[] strs) {
-        return null;
+        Map<String, List<String>> map = new HashMap<>();
+        for (int i = 0; i < strs.length; i++) {
+            char[] chars = strs[i].toCharArray();
+            Arrays.sort(chars);
+            List<String> list = map.getOrDefault(String.valueOf(chars),new ArrayList<>());
+            list.add(strs[i]);
+            map.put(String.valueOf(chars),list);
+        }
+        return new ArrayList<>(map.values());
     }
 }
