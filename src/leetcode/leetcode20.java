ce9640d937f34adf97e6bbadae44e5dc2d0204e9
cela <1319239848@qq.com>
2020-12-19 17:03:49 +0800

    栈解决有效括号

--- src/leetcode/leetcode20.java
+++ src/leetcode/leetcode20.java
@@ -1,5 +1,9 @@
 package leetcode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 /**
  * @Author ZhangKe
  * @Date 2020/8/14 11:08
@@ -7,7 +11,39 @@ package leetcode;
  * 有效的括号
  */
 public class leetcode20 {
+    public static void main(String[] args) {
+        leetcode20 l = new leetcode20();
+        l.isValid("([)]");
+    }
     public boolean isValid(String s) {
-        return false;
+        if (s == null || "".equals(s)) {
+            return true;
+        }
+        Stack<String> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            String str = s.substring(i,i+1);
+            if (str.equals("(") || str.equals("[") || str.equals("{")) {
+                stack.push(str);
+            }else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                String s1 = stack.pop();
+                if (str.equals(")")) {
+                    if (!s1.equals("(")) {
+                        return false;
+                    }
+                }else if (str.equals("]")){
+                    if (!s1.equals("[")) {
+                        return false;
+                    }
+                }else {
+                    if (!s1.equals("{")) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return stack.isEmpty();
     }
 }
