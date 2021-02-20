3861f80788eab70a4725113f50aa05268856159a
cela <1319239848@qq.com>
2021-02-20 14:38:22 +0800

    动态规划解决回文子串

---
+++ src/leetcode/leetcode647.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+/**
+ * @Author ZhangKe
+ * @Date 2021/2/20 14:37
+ * @Version 1.0
+ * 回文子串
+ */
+public class leetcode647 {
+    public int countSubstrings(String s) {
+        boolean[][] dp = new boolean[s.length()][s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = 0; j <= i; j ++) {
+                if (s.charAt(i) == s.charAt(j) &&(i - j <= 2 || dp[j+1][i-1])) {
+                    dp[j][i] = true;
+                }
+            }
+        }
+        int res = 0;
+        for (int i = 0; i < s.length();i++) {
+            for (int j = 0; j < s.length(); j++) {
+                if (dp[i][j]) {
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+}
