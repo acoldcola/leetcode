3f68db92b26e964ad3889823c9212962b4285534
cela <1319239848@qq.com>
2021-02-22 17:16:32 +0800

    动态规划解决最长公共子序列

--- src/leetcode/leetcode1143.java
+++ src/leetcode/leetcode1143.java
@@ -4,6 +4,22 @@ package leetcode;
  * @Author ZhangKe
  * @Date 2020/4/25 14:53
  * @Version 1.0
+ * 最长公共子序列
  */
 public class leetcode1143 {
+    public static int longestCommonSubsequence(String text1, String text2) {
+        char[] s1 = text1.toCharArray();
+        char[] s2 = text2.toCharArray();
+        int[][] dp = new int[s1.length + 1][s2.length + 1];
+        for (int i = 1; i < s1.length; i++) {
+            for (int j = 1; j < s2.length; j++) {
+                if (s1[i - 1] == s2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[s1.length][s2.length];
+    }
 }
