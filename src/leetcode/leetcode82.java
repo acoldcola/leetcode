0a00545c9acc4302b39622388318df3d2c084f02
cela <1319239848@qq.com>
2021-03-25 11:02:41 +0800

    删除排序链表中的重复元素 II

--- src/leetcode/leetcode82.java
+++ src/leetcode/leetcode82.java
@@ -1,17 +1,42 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @auther ZhangKe
  * @date 2019/2/22 22:29
+ * 删除排序链表中的重复元素 II
  */
 public class leetcode82 {
     public ListNode deleteDuplicates(ListNode head) {
-        ListNode h = head;
-        while (h.next != null) {
-            if (h.val == h.next.val) {
-                h = h.next.next;
+        if (head == null) {
+            return null;
+        }
+        ListNode p = head;
+        Map<Integer, Integer> map = new HashMap<>();
+        while (p != null) {
+            map.put(p.val,map.getOrDefault(p.val,0) + 1);
+            p = p.next;
+        }
+        while (head != null) {
+            if (map.get(head.val) > 1) {
+                head = head.next;
+            }else {
+                break;
             }
-            h = h.next;
+        }
+        if (head == null) {
+            return null;
+        }
+        p = head;
+        while (p.next != null) {
+            ListNode node = p.next;
+            if (map.get(node.val) > 1) {
+                p.next = node.next;
+            } else {
+                p = p.next;
+            }
         }
         return head;
     }
