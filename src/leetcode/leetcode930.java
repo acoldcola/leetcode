a3eed67fc977d3bdee27e35abd98dceb9aec04e1
cela <1319239848@qq.com>
2021-07-08 23:10:05 +0800

    提高效率

--- src/leetcode/leetcode930.java
+++ src/leetcode/leetcode930.java
@@ -1,12 +1,15 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @Author ZhangKe
  * @Date 2021/7/8 22:08
  * @Version 1.0
  */
 public class leetcode930 {
-    public int numSubarraysWithSum(int[] nums, int goal) {
+    /*public int numSubarraysWithSum(int[] nums, int goal) {
         int[] a = new int[nums.length+1];
         a[0] = 0;
         int sum = 0;
@@ -24,5 +27,17 @@ public class leetcode930 {
             }
         }
         return sum;
+    }*/
+
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int sum = 0;
+        int res = 0;
+        for (Integer a : nums) {
+            map.put(sum,map.getOrDefault(sum,0)+1);
+            sum += a;
+            res += map.getOrDefault(sum - goal,0);
+        }
+        return res;
     }
 }
